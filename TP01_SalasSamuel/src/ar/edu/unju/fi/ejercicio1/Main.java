49906d960d4194c56f832626b1ef0ad12761eca3
samuel-cell <salassamuel098@gmail.com>
2024-04-04 16:46:36 -0300

    Ejercicio 01 agregado

--- TP01_SalasSamuel/src/ar/edu/unju/fi/ejercicio1/Main.java
+++ TP01_SalasSamuel/src/ar/edu/unju/fi/ejercicio1/Main.java
@@ -3,8 +3,9 @@ package ar.edu.unju.fi.ejercicio1;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hola Mundo");
+		int n1 = 8, n2 = 5, n3 = 1, n4 = 20, n5 = 9;
+		double promedio = (double)(n1+n2+n3+n4+n5)/5;
+		System.out.println(n1+" "+n2+" "+n3+" "+n4+" "+n5);
+		System.out.print("El Promedio es: "+promedio);
 	}
-
 }
--- TP01_SalasSamuel/src/module-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * 
- */
-/**
- * 
- */
-module TP01_SalasSamuel {
-}
