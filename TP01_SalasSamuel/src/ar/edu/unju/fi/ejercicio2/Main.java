d241919672b21c17217ff96eb8e254e29a2653dc
samuel-cell <salassamuel098@gmail.com>
2024-04-13 23:12:30 -0300

    ejercicio 2 enviado al repositorio

---
+++ TP01_SalasSamuel/src/ar/edu/unju/fi/ejercicio2/Main.java
@@ -0,0 +1,20 @@
+package ar.edu.unju.fi.ejercicio2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		String nombrePais = "Peru";
+        int edadPersona = 26;
+        double alturaEdificio = 90.5;
+        double precioProducto = 9.99;
+        String numeroTelefono = "2233211221";
+        double coseno = Math.cos(0.5);
+        System.out.println("Nombre del país: " + nombrePais);
+        System.out.println("Edad de la persona: " + edadPersona);
+        System.out.println("Altura del edificio: " + alturaEdificio + " metros");
+        System.out.println("Precio del producto: $" + precioProducto);
+        System.out.println("Número de teléfono: " + numeroTelefono);
+        System.out.println("Cálculo del coseno de 0.5: " + coseno);
+	}
+
+}
