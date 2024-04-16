363499c21e230d59b29b03a69d230037d9cf0910
samuel-cell <salassamuel098@gmail.com>
2024-04-16 00:19:26 -0300

    .

--- TP01_SalasSamuel/src/ar/edu/unju/fi/ejercicio10/Main.java
+++ TP01_SalasSamuel/src/ar/edu/unju/fi/ejercicio10/Main.java
@@ -5,5 +5,43 @@ public class Main {
 
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
+		
+		Pizza pizza1=new Pizza();
+		System.out.println("Ingrese el diametro de la pizza 1");
+		pizza1.setDiametro(sc.nextInt());
+		while(pizza1.getDiametro()!=20&&pizza1.getDiametro()!=30&&pizza1.getDiametro()!=40) {
+			System.out.println("Diametro de pizza invalido, solo se admiten los valores [20-30-40] Intente denuevo");
+			pizza1.setDiametro(sc.nextInt());
+		}
+		System.out.println("Ingrese si quiere ingredientes especiales a la pizza [true/false]");
+		pizza1.setIngredientesEspeciales(sc.nextBoolean());
+		pizza1.setArea(pizza1.calcularArea());
+		pizza1.setPrecio(pizza1.calcularPrecio());
+		
+		Pizza pizza2=new Pizza();
+		System.out.println("Ingrese el diametro de la pizza 2");
+		pizza2.setDiametro(sc.nextInt());
+		while(pizza2.getDiametro()!=20&&pizza2.getDiametro()!=30&&pizza2.getDiametro()!=40) {
+			System.out.println("Diametro de pizza invalido, solo se admiten los valores [20-30-40] Intente denuevo");
+			pizza2.setDiametro(sc.nextInt());
+		}
+		System.out.println("Ingrese si quiere ingredientes especiales a la pizza [true/false]");
+		pizza2.setIngredientesEspeciales(sc.nextBoolean());
+		pizza2.setArea(pizza2.calcularArea());
+		pizza2.setPrecio(pizza2.calcularPrecio());
+		
+		Pizza pizza3=new Pizza();
+		System.out.println("Ingrese el diametro de la pizza 3");
+		pizza3.setDiametro(sc.nextInt());
+		while(pizza3.getDiametro()!=20&&pizza3.getDiametro()!=30&&pizza3.getDiametro()!=40) {
+			System.out.println("Diametro de pizza invalido, solo se admiten los valores [20-30-40] Intente denuevo");
+			pizza3.setDiametro(sc.nextInt());
+		}
+		System.out.println("Ingrese si quiere ingredientes especiales a la pizza [true/false]");
+		pizza3.setIngredientesEspeciales(sc.nextBoolean());
+		pizza3.setArea(pizza3.calcularArea());
+		pizza3.setPrecio(pizza3.calcularPrecio());
+		
+        sc.close();
 	}
 }
--- TP01_SalasSamuel/src/ar/edu/unju/fi/ejercicio10/Model/Pizza.java
+++ TP01_SalasSamuel/src/ar/edu/unju/fi/ejercicio10/Model/Pizza.java
@@ -1,29 +1,60 @@
 package ar.edu.unju.fi.ejercicio10.Model;
 
 public class Pizza {
-	private double diameter;
-	private double price;
+	private int diametro;
+	private float precio;
 	private double area;
-	private boolean special_ingredients;
-	private final int special_in_20 = 500;
-	private final int special_in_30 = 700;
-	private final int special_in_40 = 1000;
-	
-	public Pizza() {
-		
+	private boolean ingredientesEspeciales;
+	final short INGREDIENTES_ESPECIALES_20=500;
+	final short INGREDIENTES_ESPECIALES_30=750;
+	final short INGREDIENTES_ESPECIALES_40=1000;
+	public int calcularPrecio() {
+		switch(diametro) {
+			case 20:
+				if(ingredientesEspeciales==true) {
+					return 4500+INGREDIENTES_ESPECIALES_20;
+				}
+				else {
+					return 4500;
+				}
+			case 30:
+				if(ingredientesEspeciales==true) {
+					return 4800+INGREDIENTES_ESPECIALES_30;
+				}
+				else {
+					return 4800;
+				}
+			case 40:
+				if(ingredientesEspeciales==true) {
+					return 5500+INGREDIENTES_ESPECIALES_40;
+				}
+				else {
+					return 5500;
+				}
+		}
+		return 0;
 	}
-	
-	public double getDiameter() {
-		return diameter;
+	public double calcularArea(){
+		area=Math.PI*(Math.pow((diametro/2), 2));
+		return area;
 	}
-	public void setDiameter(double diameter) {
-		this.diameter = diameter;
+	public void mostrarDatos() {
+		System.out.println("Diametro = "+diametro);
+		System.out.println("Ingredientes especiales ="+ingredientesEspeciales);
+		System.out.println("Precio Pizza = "+precio +"$");
+		System.out.println("Area de la pizza ="+area);
 	}
-	public double getPrice() {
-		return price;
+	public int getDiametro() {
+		return diametro;
 	}
-	public void setPrice(double price) {
-		this.price = price;
+	public void setDiametro(int diametro) {
+		this.diametro = diametro;
+	}
+	public float getPrecio() {
+		return precio;
+	}
+	public void setPrecio(float precio) {
+		this.precio = precio;
 	}
 	public double getArea() {
 		return area;
@@ -31,23 +62,10 @@ public class Pizza {
 	public void setArea(double area) {
 		this.area = area;
 	}
-	public boolean isSpecial_ingredients() {
-		return special_ingredients;
+	public boolean isIngredientesEspeciales() {
+		return ingredientesEspeciales;
 	}
-	public void setSpecial_ingredients(boolean special_ingredients) {
-		this.special_ingredients = special_ingredients;
-	}
-	
-	public double calcularPrecio(int option, boolean special) {
-		double precioBase = 0;
-		double precioEspecial = 0;
-		
-		switch (option) {
-			case 1:
-				precioBase = 4500;
-				if(special) {
-					
-				}
-		}
+	public void setIngredientesEspeciales(boolean ingredientesEspeciales) {
+		this.ingredientesEspeciales = ingredientesEspeciales;
 	}
 }
